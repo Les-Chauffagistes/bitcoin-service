0bf2cb40613affe20f78e1cc7a7b65afc593d9ac
swakraft <hugo.beets2003@gmail.com>
2026-05-14 14:14:13 +0200

    using fallback urls if rpc call fails

--- src/v1/services/market.py
+++ src/v1/services/market.py
@@ -1,7 +1,9 @@
 from src.utils.cached_query import cached_get
 
-PRICE_URL = "https://mempool.space/api/v1/prices"
+PRICE_URLS = [
+    "https://mempool.space/api/v1/prices",
+]
 
 
 async def get_price():
-    return await cached_get(PRICE_URL)
+    return await cached_get(PRICE_URLS)
--- src/v1/services/onchain.py
+++ src/v1/services/onchain.py
@@ -1,3 +1,5 @@
+from aiohttp import ClientSession
+
 from src.settings import settings
 from src.modules.bitcoin_cli.client import Client
 
@@ -8,13 +10,28 @@ client = Client(
     settings.bitcoin_rpc_password,
 )
 
+_BLOCK_REWARD_FALLBACK = "https://blockchain.info/q/bcperblock"
+_DIFFICULTY_FALLBACK = "https://blockchain.info/q/getdifficulty"
+
 
 async def get_block_reward() -> float:
-    info = await client.get_mining_info()
-    era = info.blocks // 210000
-    return 50 / (2**era)
+    try:
+        info = await client.get_mining_info()
+        era = info.blocks // 210000
+        return 50 / (2**era)
+    except Exception:
+        async with ClientSession() as session:
+            async with session.get(_BLOCK_REWARD_FALLBACK) as resp:
+                resp.raise_for_status()
+                return float(await resp.text())
 
 
 async def get_difficulty() -> float:
-    info = await client.get_mining_info()
-    return info.difficulty
+    try:
+        info = await client.get_mining_info()
+        return info.difficulty
+    except Exception:
+        async with ClientSession() as session:
+            async with session.get(_DIFFICULTY_FALLBACK) as resp:
+                resp.raise_for_status()
+                return float(await resp.text())
